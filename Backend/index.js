4335160b7491e4324a03164a0a3fab68432f0f36
Sebastien Malo Jean <e2296673@cmaisonneuve.qc.ca>
2025-02-09 23:49:32 -0500

    Ajouter les fichiers de base pour le backend de l'API de livres, y compris .gitignore, index.html, styles.css, package.json et index.js

---
+++ Backend/index.js
@@ -0,0 +1,64 @@
+// importation des librairies
+const express = require("express");
+const dotenv = require("dotenv");
+const path = require("path");
+const { log } = require("console");
+
+const server = express();
+dotenv.config();
+
+//permission d'acces aux dossiers
+const publicFile = path.join(__dirname, "public");
+server.use(express.static(publicFile));
+
+// fonction middleWare
+function auth(req, res, next) {
+  console.log("Authentification en cours...");
+  next();
+}
+
+/**
+ * Routes établie pour récuperer tous les livres de la base de données.
+ */
+// GET
+server.get("/", (req, res) => {
+  const answer = {
+    msg: "Ici, c'est la page d'index de l'API.",
+  };
+  return res.json(express.response);
+});
+
+server.get("/books", (req, res) => {
+  return res.json({
+    msg: "Ici, c'est la page pour afficher tous les livres de la bibliotheque.",
+  });
+});
+// POST
+server.post("/books", (req, res) => {
+  return res.json({ msg: "Ici, c'est la page pour la création d'un livre." });
+});
+
+server.post("/books/initialisation", (req, res) => {
+  return res.json({
+    msg: "Je ne sais pas a quoi sert la page d'initialisation page. mais elle est là.",
+  });
+});
+// PUT
+server.put("/books/:id", (req, res) => {
+  return res.json({ msg: "Ici, c'est la page pour modifier un livre." });
+});
+// DELETE
+server.delete("/books/:id", (req, res) => {
+  return res.json({
+    msg: "Ici c'est la page pour supprimer un livre de la bibliotheque.",
+  });
+});
+// ressource 404
+server.use((req, res) => {
+  res.statusCode = 404;
+  return res.json({ msg: "Ici, c'est la page d'erreur 404." });
+});
+
+server.listen(process.env.PORT, () => {
+  console.log(`Le serveur est en écoute sur le port ${process.env.PORT}`);
+});
